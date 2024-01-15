35cae74aa260bd1b2d0fa33ee2aae06f1802cdd1
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-15 18:24:40 +0600

    done

--- first_app/views.py
+++ first_app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    
-    return render(request,'first_app/index.html') 
+    d = {'name' : 'abu', 'age': 10}
+    return render(request,'first_app/index.html', context=d) 
