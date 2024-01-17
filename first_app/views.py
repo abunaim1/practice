643c3676d7b8913615a5087f2d823e7d5864f9b4
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-17 20:34:11 +0600

    context filter added

--- first_app/views.py
+++ first_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime 
 # Create your views here.
 
 def home(request):
     d = {
         'name' : 'abu naim abdullah',
         'age' : 20,
-        'courses' : ['python', 'JS', 'C']
+        'courses' : ['python', 'JS', 'C', 'C++', 'C#'],
+        'enroll_date' : datetime.datetime.now(),
+        'empty' : '',
     }
     return render(request, 'first_app/home.html',context=d)
 
--- practice/urls.py
+++ practice/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home),
     path('first_app/', include('first_app.urls')),
 ]
